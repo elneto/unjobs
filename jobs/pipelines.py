baa3e2b41639cb657178dc6a917aeab932776491
Ernesto Araiza <yo@ernestoaraiza.com>
2016-02-07 15:33:33 -0500

    saves items to mongodb in localhost 2

--- jobs/pipelines.py
+++ jobs/pipelines.py
@@ -4,12 +4,6 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-class JobsPipeline(object):
-    def process_item(self, item, spider):
-        return item
-
 import pymongo
 
 class JobsPipeline(object):
